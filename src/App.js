54f6f432431cbc1cfb13c24eadf05b3d9581b3b6
Yusuf Rizal <yusufrizalh@gmail.com>
2021-09-02 11:41:31 +0700

    materi dasar react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  // const person = { name: 'rizal', email: 'rizal@inixindo.co.id' };
+  const link = "http://inixindo.id";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{ person }</p> */}
+
+        <p>{ 10 }</p>
+        <p>{ "Hello, Inixindo" }</p>
+        <p>{ [1, 2, 3, 4, 5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link }>Inixindo Site</a>
+      </div>
     </div>
   );
 }
+
+/*
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+const element = <Welcome name="Rizal" />;
+ReactDOM.render(element, document.getElementById('root'));
+*/
 
 export default App;

085bf8bf4200764a1e6c4394298317883bb21d74
Yusuf Rizal <yusufrizalh@gmail.com>
2021-09-03 16:13:21 +0700

    materi dasar react

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,22 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum dolor amet...', author: 'yusuf', id: 1 },
-        { title: 'Welcome aboard!', body: 'lorem ipsum dolor amet...', author: 'rizal', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum dolor amet...', author: 'yusuf', id: 3 },
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('useEffect is here');
+        fetch('http://localhost:8000/blogs')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                // console.log(data);
+                setBlogs(data);
+            });
     }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }

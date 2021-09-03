cdfe3272f5ef44f2407af062c32ec2ce7e1312c3
Yusuf Rizal <yusufrizalh@gmail.com>
2021-09-03 16:34:38 +0700

    materi dasar react

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,10 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
+        setTimeout(() => {
         fetch('http://localhost:8000/blogs')
             .then(response => {
                 return response.json()
@@ -12,11 +14,14 @@ const Home = () => {
             .then(data => {
                 // console.log(data);
                 setBlogs(data);
-            });
+                setIsPending(false);
+            }); 
+        }, 2000);
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div> } 
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
